49fc34882e7bf1c398ed7b0883fb9d6454d1cad7
swetha alampally <swalampa@SWALAMPALAPTOP1>
2018-08-09 22:57:39 +0100

    Add project files.

---
+++ ConsoleAppWithJenkinsTests/ProgramTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleAppWithJenkins;
+
+namespace ConsoleAppWithJenkins.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void CreateMessageTest()
+        {
+            Assert.AreEqual("Hello its working", Program.CreateMessage());
+        }
+    }
+}
